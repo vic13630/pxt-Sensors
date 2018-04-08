3d1e2f4a48c58afcb0317a6909bc0ab7e950e1e9
vic13630 <37115306+vic13630@users.noreply.github.com>
2018-04-08 22:25:29 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,36 @@
+/**
+* RGB三色LED共陽極/共陰極函數
+*/
+//% weight=0 color=#3CB371 icon="\uf2db" block="Sensors"
+namespace Sensors
+{
+    //% blockId=RGB_LED1 block="Common Anode RGB LED set R pin %RedPin|R value(0~255) %RedValue|G pin %GreenPin|G value(0~255) %GreenValue|B pin %BluePin|B value(0~255) %BlueValue" blockExternalInputs=false
+	//% RedValue.min=0 RedValue.max=255 GreenValue.min=0 GreenValue.max=255 BlueValue.min=0 BlueValue.max=255
+	//% RedPin.fieldEditor="gridpicker" RedPin.fieldOptions.columns=4
+	//% RedPin.fieldOptions.tooltips="false" RedPin.fieldOptions.width="300"
+	//% GreenPin.fieldEditor="gridpicker" GreenPin.fieldOptions.columns=4
+	//% GreenPin.fieldOptions.tooltips="false" GreenPin.fieldOptions.width="300"
+	//% BluePin.fieldEditor="gridpicker" BluePin.fieldOptions.columns=4
+	//% BluePin.fieldOptions.tooltips="false" BluePin.fieldOptions.width="300"
+    export function RGB_LED1(RedPin: AnalogPin, RedValue: number, GreenPin: AnalogPin, GreenValue: number, BluePin: AnalogPin, BlueValue: number): void
+	{
+        pins.analogWritePin(RedPin, pins.map(RedValue, 0, 255, 0, 1023));
+        pins.analogWritePin(GreenPin, pins.map(GreenValue, 0, 255, 0, 1023));
+        pins.analogWritePin(BluePin, pins.map(BlueValue, 0, 255, 0, 1023));
+    }
+
+    //% blockId=RGB_LED2 block="Common Cathode RGB LED set Red pin %RedPin|Red value(0~255) %RedValue|Green pin %GreenPin|Green value(0~255) %GreenValue|Blue pin %BluePin|Blue value(0~255) %BlueValue" blockExternalInputs=false
+	//% RedValue.min=0 RedValue.max=255 GreenValue.min=0 GreenValue.max=255 BlueValue.min=0 BlueValue.max=255
+	//% RedPin.fieldEditor="gridpicker" RedPin.fieldOptions.columns=4
+	//% RedPin.fieldOptions.tooltips="false" RedPin.fieldOptions.width="300"
+	//% GreenPin.fieldEditor="gridpicker" GreenPin.fieldOptions.columns=4
+	//% GreenPin.fieldOptions.tooltips="false" GreenPin.fieldOptions.width="300"
+	//% BluePin.fieldEditor="gridpicker" BluePin.fieldOptions.columns=4
+	//% BluePin.fieldOptions.tooltips="false" BluePin.fieldOptions.width="300"
+    export function RGB_LED2(RedPin: AnalogPin, RedValue: number, GreenPin: AnalogPin, GreenValue: number, BluePin: AnalogPin, BlueValue: number): void
+	{
+        pins.analogWritePin(RedPin, pins.map(RedValue, 0, 255, 0, 1023));
+        pins.analogWritePin(GreenPin, pins.map(GreenValue, 0, 255, 0, 1023));
+        pins.analogWritePin(BluePin, pins.map(BlueValue, 0, 255, 0, 1023));
+    }
+}
